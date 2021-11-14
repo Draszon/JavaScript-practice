b2e9f5d4bed5be42fc42d58fd6ffbc4ab3907fff
Peter <peti0202@windowslive.com>
2021-11-14 23:46:15 +0100

    practice

---
+++ gyak.js
@@ -0,0 +1,38 @@
+let cars = {
+    'audi': {
+        'type': 'A6',
+        'color': 'red',
+        'max speed': 350,
+        'horse power': 200,
+        'used car': true,
+        'owner': null,
+        'km clock': {
+            '2000': 142000,
+            '2005': 180520
+        }
+    },
+    'volkswagen': {
+        'type': 'Golf',
+        'color': 'white',
+        'max speed': 230,
+        'horse power': 170,
+        'used car': false,
+        'owner': 'Kiss Béla',
+        'km clock': {
+            '2000': 143200,
+            '2005': 152520
+        }
+    },
+    'ford': {
+        'type': 'focus',
+        'color': 'black',
+        'max speed': 245,
+        'horse power': 175,
+        'used car': true,
+        'owner': null,
+        'km clock': {
+            '2000': 112000,
+            '2005': 114520
+        }
+    }
+}
