6dc59646c11142c91b0b41de81d206ad3fab7294
Peter <peti0202@windowslive.com>
2021-11-21 20:44:59 +0100

    practice

---
+++ bevezeto-a-gyakorlofeladatokhoz/app.js
@@ -0,0 +1,35 @@
+function loaded() {
+
+    var canvas = document.getElementById('myCanvas');
+    var context = canvas.getContext('2d');
+
+    //context.fillStyle = 'black';
+    
+    // context.fillRect(125,50,200,200);
+
+    // context.beginPath(); //a láthatatlan vonalat tiszta lappal indítom
+    // context.moveTo(55,70); //a tollat erre a koordinátára mozgatom
+    // context.lineTo(150,100); // és vonalat húzok ebbe a koordinátába
+    // context.strokeStyle = 'orange'; //ilyen színnel lesz kitöltve
+    // context.stroke(); //megrajzolom a vonalat ("tinta metódus")
+
+    // context.beginPath();
+    // context.moveTo(0,300);
+    // context.lineTo(450,0);
+    // context.strokeStyle = 'red';
+    // context.stroke();
+
+    // var contextWidth = canvas.width;
+    // var contextHeight = canvas.height;
+    // context.fillRect(contextWidth / 2, contextHeight / 2, contextWidth / 2, contextHeight / 2);
+
+    // var contextWidth = canvas.width;
+    // var contextHeight = canvas.height;
+    // var size = 100;
+    // var padding = 10;
+    // context.fillStyle = 'green';
+    // context.fillRect(contextWidth - size - padding, contextHeight - size - padding, size, size);
+
+
+
+}
