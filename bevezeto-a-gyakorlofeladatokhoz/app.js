a29c5cd245090c881d43eacfedd4cf4f8ae01a7e
Peter <peti0202@windowslive.com>
2021-11-22 23:38:13 +0100

    practice

--- bevezeto-a-gyakorlofeladatokhoz/app.js
+++ bevezeto-a-gyakorlofeladatokhoz/app.js
@@ -30,6 +30,71 @@ function loaded() {
     // context.fillStyle = 'green';
     // context.fillRect(contextWidth - size - padding, contextHeight - size - padding, size, size);
 
+    // var canvasWidth = canvas.width;
+    // var canvasHeight = canvas.height;
+    // var size = 100;
+    // var positionX = canvasWidth / 2 - size * 0.75;
+    // var positionY = canvasHeight / 2 - size * 0.75;
+    // context.fillStyle = 'rgba(255,0,0,.5)';
+    // context.fillRect(positionX, positionY, size, size);
+    // positionX += size / 2;
+    // positionY += size / 2;
+    // context.fillStyle = 'rgba(0,0,255,.5)';
+    // context.fillRect(positionX, positionY, size, size);
 
+    // var canvasWidth = canvas.width;
+    // var canvasHeight = canvas.height;
+    // var rectWidth = 250;
+    // var rectHeight = 50;
+    // var positionX = (canvasWidth - rectWidth) / 2;
+    // var positionY = (canvasHeight - rectHeight) / 2;
+    // context.fillStyle = 'red';
+    // context.fillRect(positionX, positionY - rectHeight, rectWidth, rectHeight);
+    // context.fillStyle = 'white';
+    // context.fillRect(positionX, positionY, rectWidth, rectHeight);
+    // context.fillStyle = 'green';
+    // context.fillRect(positionX, positionY + rectHeight, rectWidth, rectHeight);
 
+    // var canvasWidth = canvas.width;
+    // var canvasHeight = canvas.height;
+    // context.beginPath();
+    // context.moveTo(canvasWidth - canvasWidth, canvasHeight - canvasHeight);
+    // context.lineTo(canvasWidth, canvasHeight);
+    // context.strokeStyle = 'red';
+    // context.stroke();
+    // context.beginPath();
+    // context.moveTo(canvasWidth - canvasWidth, canvasHeight);
+    // context.lineTo(canvasWidth, canvasHeight - canvasHeight);
+    // context.strokeStyle = 'red';
+    // context.stroke();
+
+    // var canvasWidth = canvas.width;
+    // var canvasHeight = canvas.height;
+    // var size = 150;
+    // var positionX = 150;
+    // var positionY = 225;
+    // context.beginPath();
+    // context.moveTo(canvasWidth / 2, positionY - positionX);
+    // context.lineTo((canvasWidth - canvasWidth) + positionX, positionY);
+    // context.lineTo(canvasWidth - positionX, positionY);
+    // context.lineTo(canvasWidth / 2, positionY - positionX);
+    // context.strokeStyle = 'rgba(128,128,128,.5)';
+    // context.stroke();
+    // context.fillStyle = 'rgba(255,165,0,.5)';
+    // context.fill();
+
+    // var canvasWidth = canvas.width;
+    // var canvasHeight = canvas.height;
+    // context.beginPath();
+    // context.moveTo(canvasWidth / 2, canvasHeight - canvasHeight);
+    // context.lineTo(canvasWidth / 2, canvasHeight);
+    // context.strokeStyle = 'green';
+    // context.stroke();
+    // context.beginPath();
+    // context.moveTo(canvasWidth - canvasWidth, canvasHeight / 2);
+    // context.lineTo(canvasWidth, canvasHeight / 2);
+    // context.strokeStyle = 'red';
+    // context.stroke();
+
+    
 }
