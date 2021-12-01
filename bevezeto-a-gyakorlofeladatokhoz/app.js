7a182eea6c4ff1f0b8f693f35b79bb6383efa587
Peter <peti0202@windowslive.com>
2021-12-01 23:00:27 +0100

    function practice

--- bevezeto-a-gyakorlofeladatokhoz/app.js
+++ bevezeto-a-gyakorlofeladatokhoz/app.js
@@ -237,7 +237,41 @@ function loaded() {
     //     context.stroke();
     // }
 
-    //17.feladat
-    
-    
+    //17.feladat (függvények gyakorlása)
+    // var canvasWidth = canvas.width;
+    // var canvasHeight = canvas.height;
+    // function triangle (positionX, positionY, size) {
+    //     context.beginPath();
+    //     context.moveTo(positionX, positionY);
+    //     context.lineTo(positionX - size / 2, positionY + size);
+    //     context.lineTo(positionX + size / 2, positionY + size);
+    //     context.lineTo(positionX, positionY);
+    //     context.strokeStyle = 'rgba(0,0,0,.5)';
+    //     context.fillStyle = 'rgba(255,165,0,.5)';
+    //     context.stroke();
+    //     context.fill();
+    // }
+    // triangle(230, 160, 50);
+    // triangle(270, 100, 50);
+    // triangle(200, 50, 150);
+
+    //18.feladat
+    // function star (positionX, positionY, size) {
+    //     context.beginPath();
+    //     context.moveTo(positionX, positionY);
+    //     context.lineTo(positionX + size, positionY);
+    //     context.lineTo(positionX + size * 0.15, positionY + size * 0.5);
+    //     context.lineTo(positionX + size * 0.5, positionY - size * 0.4);
+    //     context.lineTo(positionX + size * 0.85, positionY + size * 0.5);
+    //     context.lineTo(positionX, positionY);
+    //     context.strokeStyle = 'rgb(233,159,184)';
+    //     context.fillStyle = 'rgb(233,159,184)';
+    //     context.fill();
+    //     context.stroke();
+    // }
+
+    // star(40, 50, 75);
+    // star(130, 120, 100);
+    // star(250, 220, 150);
+        
 }
