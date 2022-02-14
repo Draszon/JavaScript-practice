59ab60a4af2a65c173c8562b0dfd154abc3bbdb7
Szentgyörgyi Péter <peti0202@windowslive.com>
2022-02-14 21:44:11 +0100

    jQuery evenet listener gyakorlás

---
+++ bevezeto-a-gyakorlofeladatokhoz/jquery-gyak.js
@@ -0,0 +1,19 @@
+/*$(document).ready(function () {
+    $("p").on({
+        mouseover: function() {
+            $(this).hide();
+        },
+        mouseout: function() {
+            $(this).show();
+        }
+    });
+});*/
+
+/*$(document).ready(function() {
+    $("ul").on("click", "li", function() {
+        $(this).hide();
+    });
+    $("button").click(function() {
+        $("li:last").after("<li>Todooo</li>");
+    });
+});*/
