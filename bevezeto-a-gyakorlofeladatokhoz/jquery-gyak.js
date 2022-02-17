67d2fdabe081d62356c82a9af5b936964b3b16c6
Szentgyörgyi Péter <peti0202@windowslive.com>
2022-02-17 22:40:03 +0100

    jQuerry gyakorlása

--- bevezeto-a-gyakorlofeladatokhoz/jquery-gyak.js
+++ bevezeto-a-gyakorlofeladatokhoz/jquery-gyak.js
@@ -17,3 +17,46 @@
         $("li:last").after("<li>Todooo</li>");
     });
 });*/
+
+
+/*$(document).ready(function () {
+    $("button").on({
+        mouseover: function() {
+            $("span").text("Múkodik!");
+        },
+        mouseout: function() {
+            $("span").text("Ez is múkodik!");
+        },
+        click: function() {
+            $("span").text("Jéé, csak nem rámkattintottál! :O");
+        }
+    });
+});*/
+
+// elemek animálása
+/*$(document).ready(function(){
+    $("button").click(function(){
+      $("#red").animate({left: "300px"});
+    });
+});*/
+
+// több művelet végrehajtása egymás után (callback függvény, ha az egyik effek
+// teljesen lefutott, utána indul a következő)
+/*$(document).ready(function(){
+    $("button").click(function(){
+        $(this).fadeOut("slow", function(){
+            alert("Mission completed!");
+        });
+    });
+});
+
+// metódusok összefűzése másik metódussal chaining
+$(document).ready(function() {
+    $("div").click(function() {
+        $(this)
+            .animate({height: "50px"})
+            .animate({width: "50px"})
+            .animate({height: "150px"})
+            .animate({width: "150px"});
+    });
+});*/
