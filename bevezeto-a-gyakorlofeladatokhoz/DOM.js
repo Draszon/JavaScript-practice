febd6fc1057c4a153deafe09620bf7953fd927e4
Szentgyörgyi Péter <peti0202@windowslive.com>
2022-02-26 13:23:12 +0100

    update

--- bevezeto-a-gyakorlofeladatokhoz/DOM.js
+++ bevezeto-a-gyakorlofeladatokhoz/DOM.js
@@ -1,5 +1,7 @@
-function kiir() {
-    const element = document.getElementById('lname').value;
+document.getElementById("button").addEventListener("click", function() {
+    const element = document.getElementById("lname").value;
     console.log(element);
     document.getElementById("text-id").innerHTML = element;
-}
+});
+
+
