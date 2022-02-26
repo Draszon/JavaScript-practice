a58bbed0f44d61cf17d381f02ad72d970c38c4ab
Szentgyörgyi Péter <peti0202@windowslive.com>
2022-02-26 17:44:16 +0100

    update practice

--- bevezeto-a-gyakorlofeladatokhoz/DOM.js
+++ bevezeto-a-gyakorlofeladatokhoz/DOM.js
@@ -1,7 +1,23 @@
 document.getElementById("button").addEventListener("click", function() {
     const element = document.getElementById("lname").value;
-    console.log(element);
-    document.getElementById("text-id").innerHTML = element;
+    if (element === "") {
+        alert("Üresen hagytad a mezőt!");
+    } else {
+        console.log(element);
+        document.getElementById("text-id").innerHTML = element;
+    }
 });
 
-
+let num = 0;
+document.getElementById("second-btn").addEventListener("click", function() {
+    
+    if (num < 1) {
+        const para = document.createElement("p");
+        para.innerHTML = "This is a paragraph";
+        document.getElementById("myDiv").appendChild(para);
+        num++;
+        console.log(num);
+    } else {
+        alert("Többször nem tudod hozzáadni!");
+    }
+});
