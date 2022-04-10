1f6ad0afc7504f9a5d9c9799bd1bcfa7db9b73e7
Szentgyörgyi Péter <peti0202@windowslive.com>
2022-04-10 18:57:33 +0200

    bugfix

--- feladatok/gyak.js
+++ feladatok/gyak.js
@@ -1,47 +1,25 @@
-let cars = {
-    'audi': {
-        'type': 'A6',
-        'color': 'red',
-        'max speed': 350,
-        'horse power': 200,
-        'used car': true,
-        'owner': null,
-        'km clock': {
-            '2000': 142000,
-            '2005': 180520
-        }
-    },
-    'volkswagen': {
-        'type': 'Golf',
-        'color': 'white',
-        'max speed': 230,
-        'horse power': 170,
-        'used car': false,
-        'owner': 'Kiss Béla',
-        'km clock': {
-            '2000': 143200,
-            '2005': 152520
-        }
-    },
-    'ford': {
-        'type': 'focus',
-        'color': 'black',
-        'max speed': 245,
-        'horse power': 175,
-        'used car': true,
-        'owner': null,
-        // 'km clock': {
-        //     '2000': 112000,
-        //     '2005': 114520
-        // }
-    },
-    'traveled kms': function(car) {
-        var km = this[car].kms;
-        this[car].kms = prompt('Add meg a kilométeróra állást ebben az autóban: ' + this[car].type);
-        if (typeof km === 'undefined') {
-            alert('Jelenlegi km óra állása: ' + this[car].kms);
-        } else {
-            alert('A mostani km óra állása: ' + this[car].kms + '. Az előző út óta megtett táv: ' + (this[car].kms - km));
-        }
+const grid = document.querySelector('.grid');
+const height = 20;
+const width = 100;
+
+class Block {
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis];
     }
 }
+
+const blocks = [
+    new Block(5, 400)
+];
+
+function addBlock() {
+    for (let i = 0; i < blocks.length; i++) {
+        const block = document.createElement('div');
+        block.classList.add('block');
+        block.style.left = blocks[i].bottomLeft[0] + 'px';
+        block.style.bottom = blocks[i].bottomLeft[1] + 'px';
+        grid.appendChild(block);
+    }
+}
+
+addBlock();
